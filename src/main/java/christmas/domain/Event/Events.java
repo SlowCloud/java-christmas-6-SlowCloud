a239921f08222c3b4552ed5157ea435f478b1424
slowcloud <sju9417@gmail.com>
2023-11-28 10:30:50 +0900

    refactor: EventFilter 클래스를 Events의 inner class로 변경

--- src/main/java/christmas/domain/Event/Events.java
+++ src/main/java/christmas/domain/Event/Events.java
@@ -32,5 +32,33 @@ public class Events {
                 .map(DiscountEvent::getDiscount)
                 .toList());
     }
+    private static class EventFilter {
+
+        private EventFilter() {
+        }
+
+        public static List<DiscountEvent> getDiscountEvents(List<Event> events) {
+            return getFilteredEvents(events, DiscountEvent.class);
+        }
+
+        public static List<GiveawayEvent> getGiveawayEvents(List<Event> events) {
+            return getFilteredEvents(events, GiveawayEvent.class);
+        }
+
+        private static <T extends Event> List<T> getFilteredEvents(List<Event> events, Class<T> eventType) {
+            return events.stream()
+                    .filter(eventType::isInstance)
+                    .map(eventType::cast)
+                    .toList();
+        }
+
+        public static List<DiscountEvent> getDiscountEventsNotGiveawayEvent(List<Event> events) {
+            return events.stream()
+                    .filter(event -> !(event instanceof GiveawayEvent))
+                    .map(event -> (DiscountEvent) event)
+                    .toList();
+        }
+
+    }
 
 }
